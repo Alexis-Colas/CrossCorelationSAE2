7f6133a6352b2d14b967023eb2396677b1223fd9
Suzanne Deloffre <suzanne.deloffre@edu.univ-fcomte.fr>
2025-01-06 13:20:16 +0100

    CrossCorrelation2.java

--- CrossCorrelation2.java
+++ CrossCorrelation2.java
@@ -1,10 +1,42 @@
 public class CrossCorrelation2 {
 
     public static double[] crosscorrelation(double[] sig1, double[] sig2){
-        System.out.println("Hello, World!");
+        int n = sig2.length;
+        double[] cross = new double[n];
+        double [] sig3 = new double[n];
+        double sum = 0.0;
+        int gap =0;
+
+        for (int t=0; t<n; t++) {
+
+            for (int k=0; k<n; k++){
+                while (k<gap)
+                    sig3[k]=0.0;
+
+                sig3[k]=sig2[k-gap];
+
+                sum += sig1[k]*sig3[k];
+            }
+            gap++;
+
+            cross[t] = sum;
+            sum = 0.0;
+
+        }
+
+        return cross;
+    }
+
+    public static void afficheTab(double[] tab){
+        for (int i = 0; i < tab.length; i++) {
+            System.out.println(tab[i]);
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        double[] sig1 = {0.4, 0.4, 0.2, 0.7, 0.8};
+        double[] sig2 = {0.8, 0.9, 0.4, 0.4, 0.2};
+        afficheTab(crosscorrelation(sig1, sig2));
+
     }
 }
