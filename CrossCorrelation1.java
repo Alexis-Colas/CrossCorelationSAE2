b735d580768eb162680761fcfcef1c1a64bf5fb7
Alexis-Colas <alexis.colas@edu.univ-fcomte.fr>
2025-01-06 09:19:31 +0100

    Add files via upload

---
+++ CrossCorrelation1.java
@@ -0,0 +1,10 @@
+public class CrossCorrelation1 {
+
+    public static double[] crosscorrelation(double[] sig1, double[] sig2){
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
